1f0f55eb007315cbd0a78049910e2f493e94969c
Felipe <felipe@visionar.io>
2024-11-23 17:27:13 -0300

    add sections meal details screen

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -6,16 +6,52 @@ class MealDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final meal = ModalRoute.of(context)?.settings.arguments as Meal;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(meal.title),
-      ),
-      body: Center(
-        child: Text(meal.title),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(meal.title),
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Ingredientes',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+            Container(
+              width: 250,
+              height: 250,
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(color: Colors.white, border: Border.all(color: Colors.grey), borderRadius: BorderRadius.circular(10)),
+              child: ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (ctx, idx) {
+                  return Card(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 15,
+                      ),
+                      child: Text(meal.ingredients[idx]),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ));
   }
 }
